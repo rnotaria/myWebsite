81fbad5c4c6a887731a028e8e47cac42de60e11f
unknown <rnotaria776@gmail.com>
2020-10-19 23:10:40 -0500

    updated option bar

--- my-site/src/Components/Options.js
+++ my-site/src/Components/Options.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HiOutlineMail, HiOutlineDocument } from "react-icons/hi";
 import { AiOutlineGithub } from "react-icons/ai";
 import styles from "../Styles/Options.module.css";
 
 function Mail() {
+  const [text, setText] = useState("Send me an email");
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied === true) {
+      setTimeout(() => {
+        setCopied(false);
+        setText("Send me an email");
+      }, 5000);
+    }
+  });
+
+  if (copied) {
+    return (
+      <div className={styles.optionContainer}>
+        <HiOutlineMail
+          className={styles.optionIcon}
+          size={40}
+          onClick={() => {
+            navigator.clipboard.writeText("rnotaria776@gmail.com");
+            setText("Copied!");
+          }}
+        />
+        <div className={styles.optionText}>{text}</div>
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.option}>
-      <HiOutlineMail size={40} />
-      <div className={styles.optionText}>Send me an email</div>
+    <div className={styles.optionContainer}>
+      <HiOutlineMail
+        className={styles.optionIcon}
+        size={40}
+        onMouseEnter={() => setText("Click to copy email")}
+        onMouseLeave={() => setText("Send me an email")}
+        onClick={() => {
+          navigator.clipboard.writeText("rnotaria776@gmail.com");
+          setText("Copied!");
+          setCopied(true);
+        }}
+      />
+      <div className={styles.optionText}>{text}</div>
     </div>
   );
 }
 
 function Resume() {
   return (
-    <div className={styles.option}>
-      <HiOutlineDocument size={40} />
+    <div className={styles.optionContainer}>
+      <HiOutlineDocument className={styles.optionIcon} size={40} />
       <div className={styles.optionText}>Download my resume</div>
     </div>
   );
@@ -23,8 +61,8 @@ function Resume() {
 
 function GitHub() {
   return (
-    <div className={styles.option}>
-      <AiOutlineGithub size={40} />
+    <div className={styles.optionContainer}>
+      <AiOutlineGithub className={styles.optionIcon} size={40} />
       <div className={styles.optionText}>Check out my GitHub</div>
     </div>
   );
